aa6b8aaceb37dc995a8cd7049c654987b8b9cf9b
Gabriel Magreti <gabrielmagreti@gmail.com>
2021-10-16 19:06:00 -0300

    Function to import files

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,18 @@
 import { Router } from 'express';
-
+import multer from 'multer';
 // Repositories
 import { CategoriesRepository } from '../modules/cars/repositories/implementations/CategoriesRepository';
 
 // Controllers
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: './tmp',
+});
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
@@ -15,6 +20,10 @@ categoriesRoutes.post('/', (request, response) => {
 
 categoriesRoutes.get('/', (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
